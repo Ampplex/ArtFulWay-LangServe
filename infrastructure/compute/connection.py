208bbd49c48c26149c0dcf5f4ea6a3c66af48e7e
Ampplex <team.amplex@gmail.com>
2025-03-21 22:27:12 +0530

    Created aws cdk infrastructure stack

--- infrastructure/compute/connection.py
+++ infrastructure/compute/connection.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 
 # Replace with your connection string
-MONGO_URI = "mongodb+srv://admin:admin087@artfulwaycluster0.q52jt.mongodb.net/Users"
+MONGO_URI = "mongodb://localhost:27017/Users"
 
 client = MongoClient(MONGO_URI)
 db = client["Users"]
